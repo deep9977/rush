e7905c2ee5cfad90a9fc0a08c1b245143b7e8682
deep9977 <166912166+deep9977@users.noreply.github.com>
2024-07-11 13:08:01 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,21 @@ fn main(){
     
     loop{
         shell::print_prompt();
-        
-        let tempinput = shell::read_command();
-        match tempinput{
-            Ok(_) =>{} ,
-            Err("no input") => continue,
-            _ =>{},
-        }
-        let input: Input = tempinput.unwrap();
 
-        
+        let input: Input;
+        if let Ok(tempinput) = shell::read_command(){
+            input = tempinput;
+        }else{
+            continue;
+        }
 
         let mut commands: HashSet<&str> = HashSet::new();
         commands.insert("exit");
         commands.insert("echo");
         commands.insert("type");
 
-        if commands.contains(input.command.as_str()){
-            Commands::handle_command(&input);
-        }
-        else {
-            println!("command not found: {}",input.command);
-            continue;
-        }
-    
+
+        Commands::handle_command(&input);
+
     }
 }
